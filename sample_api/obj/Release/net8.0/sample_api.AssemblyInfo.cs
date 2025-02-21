a5c91a798727aec53548954e73fb3d6c3332e081
--unset <bpriyan18082004@gmail.com>
2025-02-21 09:59:18 +0530

    modified css for layout and login

--- sample_api/obj/Release/net8.0/sample_api.AssemblyInfo.cs
+++ sample_api/obj/Release/net8.0/sample_api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sample_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+745d8fe4eed05f4320ef456f433e339f52c090a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("sample_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sample_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- weather/obj/Release/net8.0/weather.AssemblyInfo.cs
+++ weather/obj/Release/net8.0/weather.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("weather")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+745d8fe4eed05f4320ef456f433e339f52c090a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("weather")]
 [assembly: System.Reflection.AssemblyTitleAttribute("weather")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
