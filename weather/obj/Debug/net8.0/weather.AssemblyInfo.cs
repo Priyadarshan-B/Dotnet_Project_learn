f1c0a982b4687988d8e9185e339937ab2a545725
--unset <bpriyan18082004@gmail.com>
2025-02-26 10:17:03 +0530

    deleted unwanted files

--- weather/obj/Debug/net8.0/weather.AssemblyInfo.cs
+++ weather/obj/Debug/net8.0/weather.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("weather")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c7234971fc5ef7ebd9fdd96e11d5f89a18f6988")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+959c493e2ec8d86ff11695462a53d0932cd03900")]
 [assembly: System.Reflection.AssemblyProductAttribute("weather")]
 [assembly: System.Reflection.AssemblyTitleAttribute("weather")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
