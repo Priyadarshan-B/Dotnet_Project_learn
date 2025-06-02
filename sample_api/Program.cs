9ac5057dfef1728d9d84b38cea9bca7e1ddf4c24
--unset <bpriyan18082004@gmail.com>
2025-06-02 11:36:21 +0530

    modified frontend api config

--- sample_api/Program.cs
+++ sample_api/Program.cs
@@ -6,11 +6,16 @@ using sample_api.Services;
 using Supabase;
 using sample_api.Data;
 using MongoDB.Driver;
-using Microsoft.JSInterop.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-var supabaseUrl = "https://qoyvfgklgyvoqxrdhrzh.supabase.co";
-var supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFveXZmZ2tsZ3l2b3F4cmRocnpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIyNzg4NDMsImV4cCI6MjA1Nzg1NDg0M30.0wIFDdDRE6duzNFbeBpzCwOvvC4JXFwVvB5S5H6UVqo";
+// Get config values
+var config = builder.Configuration;
+var supabaseUrl = config["Supabase:Url"];
+var supabaseKey = config["Supabase:Key"];
+var mongoConnectionString = config["MongoDB:ConnectionString"];
+
+// Setup Supabase
 var options = new Supabase.SupabaseOptions
 {
     AutoConnectRealtime = true
@@ -19,16 +24,16 @@ var supabase = new Supabase.Client(supabaseUrl, supabaseKey, options);
 await supabase.InitializeAsync();
 builder.Services.AddSingleton(supabase);
 
-var mongoConnectionString = "mongodb+srv://priyan:Priyan%402004@cluster0.eomizaj.mongodb.net/";
-builder.Services.AddSingleton<IMongoClient>(s =>
+// Setup MongoDB
+builder.Services.AddSingleton<IMongoClient>(_ =>
     new MongoClient(mongoConnectionString));
 
-// Add services to the container.
+// Add services to the container
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<MenuService>();
 builder.Services.AddScoped<FavoritesService>();
 
-//Cors
+// CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowSpecificOrigin", policy =>
@@ -39,13 +44,12 @@ builder.Services.AddCors(options =>
 });
 
 builder.Services.AddControllers();
-builder.Services.AddSingleton<AuthService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Pipeline config
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -54,9 +58,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseCors("AllowSpecificOrigin");
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
